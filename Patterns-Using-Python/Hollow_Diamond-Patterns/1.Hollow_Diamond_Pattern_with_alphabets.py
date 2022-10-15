87a82a7728fc7b070541e3fa5f55d963a442cbbe
Bhaskar Berry <bhaskarberry90@gmail.com>
2022-10-15 16:03:38 +0530

    Typo Fix

--- Patterns-Using-Python/Hollow_Diamond-Patterns/1.Hollow_Diamond_Pattern_with_alphabets.py
+++ Patterns-Using-Python/Hollow_Diamond-Patterns/1.Hollow_Diamond_Pattern_with_alphabets.py
@@ -1,8 +1,8 @@
-# Print Hallow diamond pattern with * symbol.
+# Print Hollow diamond pattern with * symbol.
 
 n = int(input("Enter the size of the Diamond:"))
 
-print("==========Hallow Diamond pattern with stars====")
+print("==========Hollow Diamond pattern with stars====")
 for i in range(1, n+1):# n = 3 ---> 1,2, 3
 	for j in range(1, n-i+1):
 		print(" ", end="")
